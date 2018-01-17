64318ce30d2b45715c5f3c318fcd9dd5787b0f0d
alex <alexwein@gmail.com>
2018-01-17 14:40:56 -0800

    schema fix

--- database/schema/messages.sql
+++ database/schema/messages.sql
@@ -8,13 +8,14 @@
 --   OIDS=FALSE
 -- );
 
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS messages (
 "id" serial NOT NULL,
 "text" varchar(1024) NOT NULL,
 "username" varchar(1024) NOT NULL,
 "workspace_id" varchar(1024),
+"special_type" varchar(100),
 "createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
-CONSTRAINT $1_pk PRIMARY KEY ("id")
+CONSTRAINT messages_pk PRIMARY KEY ("id")
 ) WITH (
 OIDS=FALSE
 );
