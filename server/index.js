c50f249fc00bc67010205a614512cb556fe28f38
arasdean <arasdean@gmail.com>
2018-01-17 10:59:59 -0800

    dotenv changes

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const WebSocket = require('ws');
 const router = require('./routes');
 const { onConnect } = require('./webSocket');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
