1958ead9a1c81e94d3c3984980113b75a242328b
David Harlow <davidaharlow@gmail.com>
2018-01-18 11:23:17 -0800

    WIP

--- client/src/components/MessageList.jsx
+++ client/src/components/MessageList.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
 import { Container, ListGroup } from 'reactstrap';
 import MessageEntry from './MessageEntry.jsx';
-import PollEntry from './PollEntry.jsx';
+// import PollEntry from './PollEntry.jsx';
 
 //container for message components
 export default ({ messages, currentWorkSpaceId }) => (
   <div className="message-list-container">
     <Container>
       {messages.map(message => {
-      	if (message.feature === 'poll') {
-      		<PollEntry message={message} key={message.id} />
-      	} else {
       		<MessageEntry message={message} key={message.id} />	
-      	}})	
-    	}
+    	})}
     </Container>
   </div>
 );
-
 
