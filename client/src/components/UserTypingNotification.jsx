0850fb7f5d133a87ca325039638d987cb08889d4
Jake Luton <jake.luton92@gmail.com>
2018-01-20 16:38:52 -0800

    Animated user typing notification.

--- client/src/components/UserTypingNotification.jsx
+++ client/src/components/UserTypingNotification.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default (props) => {
   return props.typingUser !== null ? (
-    <span class="user-typing-notification">
+    <span className="user-typing-notification animated infinite pulse">
       {`${props.typingUser} is typing...`}
     </span>
   ) : null;
